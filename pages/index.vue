083a02f3fce1794790be4ce2ef47f1b698858120
esplito <eesplund@gmail.com>
2019-07-02 11:47:37 +0200

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="container">
+    <h2>This is a homepage</h2>
+    <div class="posts">
+
+      <h3>Latest from UPS</h3>
+      <div class="post" v-for="post in posts" :key="post.id">
+        <h4>{{ post.title.rendered }}</h4>
+        <div class="post-content" v-html="post.excerpt.rendered"></div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  fetch({ store }) {
+    return axios.get('http://api.uppsalapolitices.se/wp-json/wp/v2/posts').then((res) => {
+      store.commit('frontPagePosts', res.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts
+    }
+  }
+}
+</script>
+<style>
+.container {
+  margin: 0 auto;
+  width: 960px;
+}
+</style>
